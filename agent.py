5561b72aceaddf396f39251902740e048aa62910
Ramon Cruz <ramoncruz@ciencias.unam.mx>
2022-01-23 22:43:07 -0600

    implementacion del juego

---
+++ agent.py
@@ -0,0 +1,171 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direcciones, Punto
+from model import Linear_QNet, QTrainer
+from helper import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001 #learning rate
+
+class Agent:
+
+    def __init__(self):
+        self.n_juegos = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memoria = deque(maxlen=MAX_MEMORY) # popleft()
+        self.modelo = Linear_QNet(11, 256, 3)
+        self.entreno = QTrainer(self.modelo, lr=LR, gamma=self.gamma)
+
+
+    def obtener_estado(self, game):
+        '''
+        Toma los datos del juego en dicho momento 
+        Parameters:
+        game : instancia del juego 
+
+        Returns:
+        np.array : valores del estado
+        '''
+        cabeza = game.snake[0]
+        punto_izq = Punto(cabeza.x - 20, cabeza.y)
+        punto_der = Punto(cabeza.x + 20, cabeza.y)
+        punto_arriba = Punto(cabeza.x, cabeza.y - 20)
+        punto_abajo = Punto(cabeza.x, cabeza.y + 20)
+        
+        dir_izq = game.direccion == Direcciones.IZQUIERDA
+        dir_der = game.direccion == Direcciones.DERECHA
+        dir_arriba = game.direccion == Direcciones.ARRIBA
+        dir_abajo = game.direccion == Direcciones.ABAJO
+
+        estado = [
+            # peligro de ir hacia adelante
+            (dir_der and game.es_choque(punto_der)) or 
+            (dir_izq and game.es_choque(punto_izq)) or 
+            (dir_arriba and game.es_choque(punto_arriba)) or 
+            (dir_abajo and game.es_choque(punto_abajo)),
+
+            # peligro de ir hacia la derecha
+            (dir_arriba and game.es_choque(punto_der)) or 
+            (dir_abajo and game.es_choque(punto_izq)) or 
+            (dir_izq and game.es_choque(punto_arriba)) or 
+            (dir_der and game.es_choque(punto_abajo)),
+
+            # peligro de ir hacia la izquierda
+            (dir_abajo and game.es_choque(punto_der)) or 
+            (dir_arriba and game.es_choque(punto_izq)) or 
+            (dir_der and game.es_choque(punto_arriba)) or 
+            (dir_izq and game.es_choque(punto_abajo)),
+            
+            # movimiento de direccion
+            dir_izq,
+            dir_der,
+            dir_arriba,
+            dir_abajo,
+            
+            # lugar de la comida
+            game.comida.x < game.cabeza.x,  # comida izquierda
+            game.comida.x > game.cabeza.x,  # comida derecha
+            game.comida.y < game.cabeza.y,  # comida arriba
+            game.comida.y > game.cabeza.y  # comida abajo
+            ]
+
+        return np.array(estado, dtype=int)
+
+    def recuerdo(self, estado, accion, recompensa, sig_estado, done):
+        '''
+        Guarda todos los datos del juego en la cola para un futuro entrenamiento
+
+        Parameters:
+        estado : estado del juego arrays de los datos
+        accion : accion que se realizo
+        recompensa: recompensa que se lleva hasta ahora
+        sig_estado: estado del juego siguiente
+        '''
+        self.memoria.append((estado, accion, recompensa, sig_estado, done)) # popleft si se excede la memoria
+
+    def entrenamiento_largo(self):
+        '''
+        Entrena nuestro modelo con todas los recuerdos guardades en la cola       
+        '''
+        if len(self.memoria) > BATCH_SIZE: # si excedimos la memoria tomamos una parte random
+            mini_muestra = random.sample(self.memoria, BATCH_SIZE) # lista de tuplas
+        else:
+            mini_muestra = self.memoria # tomamos toda 
+
+        estados, acciones, recompensas, sig_estados, dones = zip(*mini_muestra) # descomprime las tuplas de la cola 
+        self.entreno.train_step(estados, acciones, recompensas, sig_estados, dones)
+
+    def entrenamiento_corto(self, estado, accion, recompensa, sig_estado, done):
+        '''
+        Entrena nuestro modelo con una sola accion
+        '''
+        self.entreno.train_step(estado, accion, recompensa, sig_estado, done)
+
+    def tomar_accion(self, estado):
+        # random movimiento: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_juegos
+        mov_final = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            mover = random.randint(0, 2)
+            mov_final[mover] = 1
+        else:
+            estado0 = torch.tensor(estado, dtype=torch.float)
+            prediction = self.modelo(estado0)
+            mover = torch.argmax(prediction).item()
+            mov_final[mover] = 1
+
+        return mov_final
+
+
+def train():
+    '''
+    Entrena el modelo
+    '''
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agente = Agent()
+    game = SnakeGameAI()
+    while True:
+        # toma el ultimo estado
+        estado_final = agente.obtener_estado(game)
+
+        # obtiene el ultimo movimiento
+        mov_final = agente.tomar_accion(estado_final)
+
+        # mejora el movimiento y obtiene un nuevo estado
+        recompensa, done, puntaje = game.dar_paso(mov_final)
+        estado_new = agente.obtener_estado(game)
+
+        # entrenamiento con corta memoria 
+        agente.entrenamiento_corto(estado_final, mov_final, recompensa, estado_new, done)
+
+        # recuerdo
+        agente.recuerdo(estado_final, mov_final, recompensa, estado_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reinicio()
+            agente.n_juegos += 1
+            agente.entrenamiento_largo()
+
+            if puntaje > record:
+                record = puntaje
+                agente.modelo.save()
+
+            print('Juego', agente.n_juegos, 'puntaje', puntaje, 'Record:', record)
+
+            plot_scores.append(puntaje)
+            total_score += puntaje
+            mean_score = total_score / agente.n_juegos
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+
+if __name__ == '__main__':
+    train()
---
+++ game.py
@@ -0,0 +1,188 @@
+import pygame #libreria para  crear el juego 
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+#font = pygame.font.SysFont('arial', 25)
+
+class Direcciones(Enum):
+    '''
+    Enum con las direcciones que puede dar el juego
+    '''
+    DERECHA = 1
+    IZQUIERDA = 2
+    ARRIBA = 3
+    ABAJO = 4
+
+Punto = namedtuple('Punto', 'x, y')
+
+# rgb colores
+BLANCO = (255, 255, 255)
+ROJO = (200,0,0)
+NEGRO = (0,0,0)
+VERDE = (0,128,0)
+VERDE2 = (0,100,0)
+
+BLOCK_SIZE = 20
+VELOCIDAD = 40
+
+class SnakeGameAI:
+
+    def __init__(self, ancho=640, alto=480):
+        '''
+        Inicializacion del objeto juego Snakegame 
+        
+        Parameters:
+        ancho : ancho de la pantalla 
+        alto : alto de la pantalla
+        '''
+        self.ancho = ancho
+        self.alto = alto
+        # Inicia la pantalla con los valores ancho y alto
+        self.pantalla = pygame.display.set_mode((self.ancho, self.alto))
+        pygame.display.set_caption('Snake')
+        self.reloj = pygame.time.Clock()
+        self.reinicio()
+
+
+    def reinicio(self):
+        '''
+        Inicio del juego se reinicia el juego desde el principio
+        '''
+        self.direccion = Direcciones.DERECHA
+
+        self.cabeza = Punto(self.ancho/2, self.alto/2)
+        self.snake = [self.cabeza,
+                      Punto(self.cabeza.x-BLOCK_SIZE, self.cabeza.y),
+                      Punto(self.cabeza.x-(2*BLOCK_SIZE), self.cabeza.y)]
+
+        self.puntaje = 0
+        self.comida = None
+        self._lugar_comida()
+        self.frame_iteration = 0 # numeros de FPS en la iteracion
+
+        
+    def _lugar_comida(self):
+        '''
+        Coloca la comida en algun random del juego
+        '''
+        x = random.randint(0, (self.ancho-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        y = random.randint(0, (self.alto-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.comida = Punto(x, y)
+        if self.comida in self.snake:
+            self._lugar_comida()
+
+
+    def dar_paso(self, accion):
+        '''
+        Da un paso en el juego haciendo que se mueva snake(vibora)
+
+        Parameters:       
+        accion : accion de va a hacer snake 
+        '''
+        self.frame_iteration += 1
+        # 1. obtiene las acciones del usuario
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        # 2. mueve
+        self._mover(accion) # actualiza la cabeza
+        self.snake.insert(0, self.cabeza)
+        
+        # 3. verifica que el juego no haya termiando
+        recompensa = 0
+        fin_juego = False
+        if self.es_choque() or self.frame_iteration > 100*len(self.snake):
+            fin_juego = True
+            recompensa = -10
+            return recompensa, fin_juego, self.puntaje
+
+        # 4. luegar de la nueva comida o solo se mueve
+        if self.cabeza == self.comida:
+            self.puntaje += 1
+            recompensa = 10
+            self._lugar_comida()
+        else:
+            self.snake.pop()
+        
+        # 5. actualiza la pantalla y el reloj
+        self._actualiza_pantalla()
+        self.reloj.tick(VELOCIDAD)
+        # 6. da fin el juego y segresa la recompensa
+        return recompensa, fin_juego, self.puntaje
+
+
+    def es_choque(self, pt=None):
+        '''
+        Informa  si choco con si mismo o alguna pared 
+        '''
+        if pt is None:
+            pt = self.cabeza
+        # si le pega al borde 
+        if pt.x > self.ancho - BLOCK_SIZE or pt.x < 0 or pt.y > self.alto - BLOCK_SIZE or pt.y < 0:
+            return True
+        # Si se pega con el mismo
+        if pt in self.snake[1:]:
+            return True
+
+        return False
+
+
+    def _actualiza_pantalla(self):
+        '''
+        Actualiza la pantalla del juego
+        '''
+        
+        self.pantalla.fill(NEGRO) #fondo
+
+        for pt in self.snake:
+            pygame.draw.rect(self.pantalla, VERDE2, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.pantalla, VERDE, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+
+        pygame.draw.rect(self.pantalla, ROJO, pygame.Rect(self.comida.x, self.comida.y, BLOCK_SIZE, BLOCK_SIZE))
+
+        text = font.render("puntaje: " + str(self.puntaje), True, BLANCO)
+        self.pantalla.blit(text, [0, 0])
+        pygame.display.flip()
+
+
+    def _mover(self, accion):
+        '''
+        Se cambia los valores del snake dependiendo la accion recibida
+
+        Parameters:
+        accion: accion que debe tomar
+        '''
+        # [ADELANTE, DERECHA, IZQUIERDA]
+
+        reloj_apunta = [Direcciones.DERECHA, Direcciones.ABAJO, Direcciones.IZQUIERDA, Direcciones.ARRIBA]
+        idx = reloj_apunta.index(self.direccion)
+
+        if np.array_equal(accion, [1, 0, 0]):
+            nueva_direc = reloj_apunta[idx] # no cambia
+        elif np.array_equal(accion, [0, 1, 0]):
+            sig_indx = (idx + 1) % 4
+            nueva_direc = reloj_apunta[sig_indx] # DERECHA da vuelta 
+        else: # [0, 0, 1]
+            sig_indx = (idx - 1) % 4
+            nueva_direc = reloj_apunta[sig_indx] # IZQUIERDA da vuelta  
+
+        self.direccion = nueva_direc
+
+        x = self.cabeza.x
+        y = self.cabeza.y
+        if self.direccion == Direcciones.DERECHA:
+            x += BLOCK_SIZE
+        elif self.direccion == Direcciones.IZQUIERDA:
+            x -= BLOCK_SIZE
+        elif self.direccion == Direcciones.ABAJO:
+            y += BLOCK_SIZE
+        elif self.direccion == Direcciones.ARRIBA:
+            y -= BLOCK_SIZE
+
+        self.cabeza = Punto(x, y)
